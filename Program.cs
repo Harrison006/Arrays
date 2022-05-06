e95822d28e6c7123fc5bb6e66257c662fd26d78c
Harrison006 <87344112+Harrison006@users.noreply.github.com>
2022-05-06 10:34:00 +1000

    .

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program {
+    static void Main(string[] args) {
+        int [] myArray = new int[10];
+        int num = 0;
+
+        Console.Write("Enter a number >");
+        num = Convert.ToInt32(Console.ReadLine());
+        myArray[0] = num;
+        Console.WriteLine(myArray[10]);
+
+            Console.ReadKey();
+    }
+}
